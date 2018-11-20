46852ab521f7253ca4f7a0109c01b65e090ec044
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-20 11:31:43 -0700

    add controller enrollment

---
+++ app/controllers/enrollments_controller.rb
@@ -0,0 +1,37 @@
+class EnrollmentsController < ApplicationController
+  def index
+    @teachers = @course.enrollments.where(role: "teacher")
+    @tas = @course.enrollments.where(role: "ta")
+    @students = @course.enrollments.where(role "student")
+  end
+
+  def new
+    @enrollment = @course.enrollments.new
+  end
+
+  def create
+    enrollment = @course.enrollments.new(enrollment_params)
+    if @enrollment.save
+      redirect_to course_enrollments_path(@course)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    enrollment = @course.enrollments.find(params[:id])
+    enrollment.destroy
+    redirect_to course_user_path
+  end
+
+  private
+    def set_course
+      @course = Course.find(params[:id])
+    end
+
+    def enrollment_params
+      params.require(:enrollment).permit(:role, :user_id)
+    end
+end
+
+
